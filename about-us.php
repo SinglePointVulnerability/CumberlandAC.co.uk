bd418487672ab9bda1207ba201fd6ccaa169ed41
Stu <my.pyne@gmail.com>
2022-10-04 21:14:45 +0100

    Background image added, index.php created and auto-refresh / forwarding included to Weebly site

--- about-us.php
+++ about-us.php
@@ -1,4 +1,4 @@
 <?php
-/* About us*/
+/* Index */
 
 ?>
---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+/* Index*/
+?>
+<html>
+    <head>
+        <meta http-equiv="refresh" content="5; url='http://cumberland-ac.weebly.com/'" />
+        <style>
+            body {
+                background-image: url("img/main-background-large.png");
+                background-size: 100%;
+                background-repeat: no-repeat;
+            }
+            div.coming-soon {
+                color: #1c87c9;
+                font-size: 2.5em;
+                position: absolute;
+                top: 560px;
+                left: 30px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="coming-soon">
+            New website coming soon :) <br />
+            <br />
+            Click <a href="http://cumberland-ac.weebly.com/">here</a> if you aren't redirected in 5 seconds
+        </div>
+    </body>
+</html>
