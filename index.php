0a871fb25e4fd0193063782aacd2f68d845b0e03
Stu <my.pyne@gmail.com>
2022-10-06 22:11:07 +0100

    style specifications moved to styles.css
    index and styles calibrated to auto-scale menu buttons on window resize (including button hover image)

--- index.php
+++ index.php
@@ -3,59 +3,19 @@
 ?>
 <html>
     <head>
-        <meta http-equiv="refresh" content="5; url='http://cumberland-ac.weebly.com/'"
-		-->
-        <style>
-			html,
-			body {
-			  margin: 0;
-			  padding: 0;
-			}
-			.banner {
-				width: 100%;
-			}
-			.page-buttons {
-				position: relative;
-			}
-			.nav-buttons {
-				position: relative;
-			}
-            body {
-                background-image: url("img/main-background-large.png");
-                background-size: 100%;
-                background-repeat: no-repeat;
-            }
-			.parent-container {
-				width: 100%;
-				height: 100%;
-				margin: 0;
-				padding: 0;
-			}
-			.page-banner {
-				position: relative;
-				width: 100%;
-				margin: 0;
-				padding: 0;
-			}
-            .coming-soon {
-                color: #1c87c9;
-                font-size: 2.5em;
-                position: relative;
-                left: 0px;
-            }
-			
-        </style>
+        <meta http-equiv="refresh" content="5; url='http://cumberland-ac.weebly.com/'" />
+		<link rel="stylesheet" type="text/css" href="style/styles.css" media="screen" />
+
     </head>
     <body>
 	<div class="parent-container">
 		<div class="page-banner">
 			<img class="banner" src="img/main-banner.png"/>
 		</div>
-		<div class="page-buttons">
-			<img class = "nav-buttons" src="img/btn-MemArea.png"/>
+		<div class="nav-buttons">
+			<img class = "btn-MemArea" src = "img/btn-MemArea.png">
 		</div>
-        <div class="coming-soon">
-            New website coming soon :) <br />
+        <div class="coming-soon">New website coming soon :) <br />
             <br />
             Click <a href="http://cumberland-ac.weebly.com/">here</a> if you aren't redirected in 5 seconds
         </div>
