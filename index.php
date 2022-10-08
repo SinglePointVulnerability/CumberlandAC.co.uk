a25885c947df8daceb023e11ecd8522442558bea
Stu <my.pyne@gmail.com>
2022-10-08 08:13:25 +0100

    Media, Calendar, Championship and Merchandise button images added (with their hover counterparts
    styles script updated to consolidate buttons scripts into one and to use display: flex to evenly distribute images
    
    index.php updated to add button images to button nav bar

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 ?>
 <html>
     <head>
-        <meta http-equiv="refresh" content="5; url='http://cumberland-ac.weebly.com/'" />
+        <meta http-equiv="refresh" content="8; url='http://cumberland-ac.weebly.com/'" />
 		<link rel="stylesheet" type="text/css" href="style/styles.css" media="screen" />
 
     </head>
@@ -13,11 +13,15 @@
 			<img class="banner" src="img/main-banner.png"/>
 		</div>
 		<div class="nav-buttons">
-			<img class = "btn-MemArea" src = "img/btn-MemArea.png">
+			<img class = "btn btn-Med" src = "img/btn-Med.png">
+			<img class = "btn btn-Cal" src = "img/btn-Cal.png">
+			<img class = "btn btn-Champ" src = "img/btn-Champ.png">
+			<img class = "btn btn-Merch" src = "img/btn-Merch.png">
+			<img class = "btn btn-MemArea" src = "img/btn-MemArea.png">
 		</div>
         <div class="coming-soon">New website coming soon :) <br />
             <br />
-            Click <a href="http://cumberland-ac.weebly.com/">here</a> if you aren't redirected in 5 seconds
+            Click <a href="http://cumberland-ac.weebly.com/">here</a> if you aren't redirected in 8 seconds
         </div>
 	</div>
     </body>
